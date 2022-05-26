290fe72a6da4b8a938a90cc54a43ee41b11b1a51
infoshabirkhan <infoshabbirkhan@gmail.com>
2022-05-26 11:58:17 +0500

    home screen 50%

--- lib/view/screens/home_screen.dart
+++ lib/view/screens/home_screen.dart
@@ -1,10 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:github_team_practice/view/widgets/my_posts_card.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: ListView(
+          padding: EdgeInsets.only(left: 20.sp, right: 20.sp),
+          children: [
+
+            SizedBox(height: 20.sp,),
+
+            Row(
+              children: [
+                Expanded(child: Text('My Blogs', style: TextStyle(fontSize: 24.sp, fontWeight: FontWeight.bold ),)),
+                Expanded(child: Align(
+
+                    alignment: Alignment.centerRight,
+                    child: SizedBox(
+                        height: 35.sp,
+                        child: Image.asset('assets/images/signupLogo.png',))))
+              ],
+            ),
+            Text('Create and post blogs',style: TextStyle(fontSize: 16.sp),),
+
+            MyPostCards(image: 'photograher.jpeg'),
+            MyPostCards(image: 'programer.jpg'),
+            MyPostCards(image: 'self_learnng.png'),
+
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/view/widgets/my_posts_card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+
+class MyPostCards extends StatelessWidget {
+  final String image;
+  const MyPostCards({Key? key,required this.image}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+
+      margin: EdgeInsets.only(top: 20.sp),
+      height: 200.sp,
+      child: Column(
+        children: [
+          const Expanded(child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text('Post date 12/05/2022'))),
+          Expanded(
+              flex: 5,
+              child: Container(
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12.sp),
+                    boxShadow: [
+                      BoxShadow(color: Colors.black.withAlpha(100,),blurRadius: 10),
+                    ]
+
+                ),
+                child: Column(
+                  children: [
+
+                    Expanded(
+                        flex: 2,
+                        child: SizedBox(
+
+                            width: double.infinity,
+                            child: ClipRRect(
+                                borderRadius: BorderRadius.only(topLeft: Radius.circular(12.sp), topRight: Radius.circular(12.sp)),
+
+
+                                child: Image.asset('assets/images/$image', fit: BoxFit.cover,)))),
+
+                    Expanded(
+                      child: Container(
+                        margin: EdgeInsets.only(left: 10.sp, right: 10.sp),
+                        child: Row(
+                          children: [
+                            const Expanded(
+                                flex: 2,
+                                child: Text('Photography Tips')),
+                            Expanded(child: Row(
+                              children: const [
+                                Expanded(child: Icon(Icons.delete)),
+                                Expanded(child: Icon(Icons.edit)),
+                                Expanded(child: Icon(Icons.share)),
+                              ],
+                            ))
+                          ],
+                        ),
+                      ),
+                    )
+
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+}
