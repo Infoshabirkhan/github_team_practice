16e7ea8e91e5799ffcba6f6932a8b0ba7b6ed157
shehriyarahmadt <shehriyarahmadt@gmail.com>
2022-05-26 17:03:02 +0500

    signup page

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:github_team_practice/view/controller/cubit/first_check_box_cubit.dart';
 import 'package:github_team_practice/view/screens/bottom_navigation_screen.dart';
 import 'package:github_team_practice/view/screens/home_screen.dart';
 import 'package:github_team_practice/view/screens/login_screen.dart';
@@ -19,24 +21,29 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       builder: (BuildContext context, Widget? child) =>
-     MaterialApp(
-       debugShowCheckedModeBanner: false,
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          // This is the theme of your application.
-          //
-          // Try running your application with "flutter run". You'll see the
-          // application has a blue toolbar. Then, without quitting the app, try
-          // changing the primarySwatch below to Colors.green and then invoke
-          // "hot reload" (press "r" in the console where you ran "flutter run",
-          // or simply save your changes to "hot reload" in a Flutter IDE).
-          // Notice that the counter didn't reset back to zero; the application
-          // is not restarted.
-          primarySwatch: Colors.blue,
-        ),
-        home: const LoginScreen(),
+     MultiBlocProvider(
+       providers: [
+         BlocProvider(create: (_) => FirstCheckBoxCubit(false)),
+       ],
+       child: MaterialApp(
+         debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+          ),
+          home: const LoginScreen(),
 
-      ),
+        ),
+     ),
 
       designSize: Size(414, 896),
     );
