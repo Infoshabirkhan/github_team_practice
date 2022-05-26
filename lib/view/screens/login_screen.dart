a98b681b3a3bad88f25fc317782d3e5f76832fac
saif-flutter <syedsaif4all@gmail.com>
2022-05-26 15:11:04 +0500

    osho

--- lib/view/screens/login_screen.dart
+++ lib/view/screens/login_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:github_team_practice/view/screens/sign_up_screen.dart';
+import 'package:github_team_practice/view/widgets/button_widget.dart';
+import 'package:github_team_practice/view/widgets/text_button_widget.dart';
+import 'package:github_team_practice/view/widgets/text_field_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -9,32 +13,28 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-          child: ListView(
+      child: ListView(
 
-              padding: EdgeInsets.only( left: 57.sp, right: 57.sp),
-              children: [
+          padding: EdgeInsets.only(left: 57.sp, right: 57.sp),
+          children: [
             // Padding(
             //   padding: EdgeInsets.only(
             //     top: 100.sp, left: 43.sp, right: 43.sp,),
             //   child: ,
             // ),
 
-
-                SizedBox(
-                  height: 100.sp,
-                ),
-                Container(
-                  color: Colors.blue,
-                  width: 193.sp,
-                  height: 101.sp,
-                  child: Image.asset('assets/images/blogimg.png'),
-
-
-                ),
+            SizedBox(
+              height: 70.sp,
+            ),
+            Container(
+              //   color: Colors.blue,
+              width: 193.sp,
+              height: 100.sp,
+              child: Image.asset('assets/images/blogimg.png'),
+            ),
             Padding(
-              padding:  EdgeInsets.only(top: 68.sp),
+              padding: EdgeInsets.only(top: 48.sp),
               child: Text(
-
                 'Login',
                 style: GoogleFonts.akayaTelivigala(
                     color: Colors.black,
@@ -62,37 +62,11 @@ class LoginScreen extends StatelessWidget {
                   fontSize: 14.sp,
                   fontWeight: FontWeight.normal),
             ),
-           SizedBox(
-             height: 6.sp,
-           ),
+            SizedBox(
+              height: 6.sp,
+            ),
 
-           TextField(
-             decoration: InputDecoration(
-               fillColor: Colors.grey[200],
-               filled: true,
-               hintText: 'xyz@gmail.com',
-               enabledBorder: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(16.r),
-                 borderSide: const BorderSide(
-                   color: Colors.transparent
-                 ),
-               ),
-               focusedBorder: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(16.r),
-                 borderSide: const BorderSide(
-                     color: Colors.transparent
-                 ),
-               ),
-               border: OutlineInputBorder(
-                 borderRadius: BorderRadius.circular(16.r),
-                 borderSide: const BorderSide(
-                     color: Colors.transparent
-                 ),
-               ),
-             ),
-           ),
-
-
+            TextFieldWidget(text: 'xyz@gmail.com'),
 
             SizedBox(
               height: 18.sp,
@@ -107,37 +81,46 @@ class LoginScreen extends StatelessWidget {
             SizedBox(
               height: 6.sp,
             ),
+            TextFieldWidget(
+              text: '*********',
+            ),
+            TextButtonWidget(text: 'Forgot Password ?', onTap: (){},),
+            ButtonWidget(text: 'Sign In', onTap: (){}, color: const Color(0xff0D638A), textColor: Colors.white),
+            SizedBox(
+              height: 32.sp,
+            ),
+            ButtonWidget(text: 'Continue with google', onTap: (){}, color: Colors.transparent, textColor: const Color(
+                0xff0D638A)),
+            SizedBox(height: 16.sp,),
+            ButtonWidget(text: 'Continue with Facebook', onTap: (){}, color: Colors.transparent, textColor: const Color(
+                0xff0D638A)),
 
-            TextField(
-              decoration: InputDecoration(
-                fillColor: Colors.grey[200],
-                filled: true,
-                hintText: '*************',
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.r),
-                  borderSide: const BorderSide(
-                      color: Colors.transparent
-                  ),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.r),
-                  borderSide: const BorderSide(
-                      color: Colors.transparent
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(16.r),
-                  borderSide: const BorderSide(
-                      color: Colors.transparent
-                  ),
-                ),
-              ),
+            SizedBox(
+              height: 53.sp,
+            ),
+            Row(
+
+              children: [
+                const Expanded(
+
+                    child:Text('Dont have an account ?',style: TextStyle(
+                       letterSpacing: 0.18
+                    ),) ),
+                Expanded(
+
+                    child: TextButtonWidget(text: 'Create an Account', onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)
+                      {
+
+                        return const SignUpScreen();
+                      }
+                      ));
+                    },))
+              ],
             )
 
+
           ]),
-        ));
+    ));
   }
 }
-
-
-
